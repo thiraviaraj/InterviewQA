c7aa1563414c5ecf3a4284f4c64ce63b6536dac8
thiraviaraj b <thiraviaraj.b@rackspace.com>
2018-04-04 21:19:54 +0530

    N Permutation And Combination for a given String

---
+++ nPermutation.js
@@ -0,0 +1,19 @@
+function print(string, n) {
+    string = string.substring(0, n);
+    var results = [];
+  
+    if (string.length === 1) {
+      results.push(string);
+      return results;
+    }
+  
+    for (var i = 0; i < string.length; i++) {
+      var firstChar = string[i];
+      var charsLeft = string.substring(0, i) + string.substring(i + 1);
+      var innerPermutations = print(charsLeft);
+      for (var j = 0; j < innerPermutations.length; j++) {
+        results.push(firstChar + innerPermutations[j]);
+      }
+    }
+    return results;
+}
